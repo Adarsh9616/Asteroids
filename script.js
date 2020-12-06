e1b03a9934d61cd8f38ea9deb44d4a42a30779cf
Adarsh <adarshv1312@gmail.com>
2020-12-07 02:07:13 +0530

    background added

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
-alert("JS Loaded");
+// alert("JS Loaded");
 
 let startBtn=document.querySelector(".btn");
+let box=document.querySelector(".box");
 
-startBtn.addEventListener("click",function(){
-    alert("Starting the game");
+let canvas=document.querySelector(".board");
+
+let tool=canvas.getContext("2d");
+canvas.height=window.innerHeight-4.5;
+canvas.width=window.innerWidth-4.5;
+let spaceImage=new Image();
+spaceImage.src="space.jpg";
+startBtn.addEventListener("click",function()
+{
+    box.style.display="none";
+    tool.fillRect(0,0,canvas.width,canvas.height);
+    tool.drawImage(spaceImage,0,0,canvas.width,canvas.height);
 });
