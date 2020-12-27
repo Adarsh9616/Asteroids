565d880915def48dec0b3798c0f9eb92adb92e1f
Adarsh <adarshv1312@gmail.com>
2020-12-28 04:23:34 +0530

    collisions added

--- script.js
+++ script.js
@@ -72,12 +72,42 @@ class Bullet
         this.y=this.y+this.velocity.y;
     }
 }
+class Particle
+{
+    constructor(x,y,radius,velocity)
+    {
+        this.x=x;
+        this.y=y;
+        this.velocity=velocity;
+        this.radius=radius;
+        this.alpha=1;
+    }
+    draw()
+    {
+        tool.save();
+        tool.globalAlpha=this.alpha;
+        tool.beginPath();
+        tool.fillstyle="white";
+        tool.arc(this.x,this.y,this.radius,0,Math.PI*2,false);
+        tool.fill();
+        tool.restore();
+    }
+    update()
+    {
+        this.draw();
+        this.x=this.x+this.velocity.x;
+        this.y=this.y+this.velocity.y;
+        this.alpha-=0.01;
+    }
+}
 let eHeight=200;
 let eWidth=200;
 let ePosX=canvas.width/2-95;
 let ePosY=canvas.height/2-100;
 let bullets=[];
 let coronas=[];
+let particles=[];
+let animId;
 function animate()
 {
     tool.clearRect(0,0,canvas.width,canvas.height);
@@ -85,11 +115,23 @@ function animate()
     tool.drawImage(spaceImage,0,0,canvas.width,canvas.height);
     let earth=new Planet(ePosX,ePosY,eWidth,eHeight); 
     earth.draw();
+    particles.forEach(function(particle,index){
+        if(particle.alpha<=0)
+        {
+            setTimeout(function(){
+                particles.splice(index,1);
+            },0);
+        }
+        else
+        {
+            particle.update();
+        } 
+    });
     let bLength=bullets.length;
     for(let i=0;i<bLength;i++)
     {
         bullets[i].update();
-        console.log(bullets);
+        //console.log(bullets);
         if(bullets[i].x<0||bullets[i].y<0||bullets[i].x>canvas.width||bullets[i].y>canvas.height)
         {
             setTimeout(function(){
@@ -102,16 +144,32 @@ function animate()
     for(let i=0;i<cLength;i++)
     {
         coronas[i].update();
-        // console.log(bullets);
-        // if(bullets[i].x<0||bullets[i].y<0||bullets[i].x>canvas.width||bullets[i].y>canvas.height)
-        // {
-        //     setTimeout(function(){
-        //         bullets.splice(i,1);
-        //     });
-
-        // }
+        //creating collisions
+        let enemy = coronas[i];
+        if(colRect(earth,enemy))
+        {
+            cancelAnimationFrame(animId);
+            alert("Game Over");
+        }
+        bullets.forEach(function(bullet,bulletIndex){
+            if(colRect(bullet,enemy))
+            {
+                //explosion
+                for(let i =0;i<enemy.width*4;i++)
+                {
+                    particles.push(new Particle(bullet.x,bullet.y,Math.random()*2,{
+                        x:((Math.random()-0.5)*Math.random()*3),
+                        y:((Math.random()-0.5)*Math.random()*3)
+                    }));
+                }
+                setTimeout(()=>{
+                    coronas.splice(i,1);
+                    bullets.splice(bulletIndex,1)
+                },0)
+            }
+        });
     }
-    requestAnimationFrame(animate);
+    animId=requestAnimationFrame(animate);
 }
 function createCorona()
 {
@@ -131,8 +189,8 @@ function createCorona()
         }
         let angle=Math.atan2(canvas.height/2-50-y,canvas.width/2-50-x);
         let velocity={
-            x:Math.cos(angle)*2,
-            y:Math.sin(angle)*2
+            x:Math.cos(angle)*1,
+            y:Math.sin(angle)*1
         }
         let corona=new Corona(x,y,100,100,velocity);
         coronas.push(corona);
@@ -149,8 +207,8 @@ startBtn.addEventListener("click",function(e)
     animate();
     createCorona();
     window.addEventListener("click",function(e){
-        console.log(e);
-        console.log("clicked");
+        //console.log(e);
+        //console.log("clicked");
         let angle=Math.atan2(e.clientY-canvas.height/2,e.clientX-canvas.width/2);
         let velocity={
             x:Math.cos(angle)*4,
@@ -161,3 +219,20 @@ startBtn.addEventListener("click",function(e)
         
     });
 });
+
+function colRect(entity1,entity2)
+{
+    let l1= entity1.x;
+    let l2= entity2.x;
+    let r1=entity1.x + entity1.width;
+    let r2=entity2.x + entity2.width;
+    let t1=entity1.y+entity1.height;
+    let t2=entity2.y+entity2.height;
+    let b1=entity1.y;
+    let b2=entity2.y;
+    if(l1<r2&&l2<r1&&t1>b2&&t2>b1)
+    {
+        return true;
+    }
+    return false;
+}
